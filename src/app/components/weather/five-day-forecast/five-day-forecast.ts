4f470daa996d6e5716175309d6fbcd2503f14ab9
mkorhankosece <mkosece@flyingdonkey.com.au>
2025-11-16 12:32:15 +0300

    Setup project

---
+++ src/app/components/weather/five-day-forecast/five-day-forecast.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-five-day-forecast',
+  imports: [],
+  templateUrl: './five-day-forecast.html',
+  styleUrl: './five-day-forecast.scss',
+})
+export class FiveDayForecast {
+
+}
---
+++ src/app/components/weather/weather-dashboard/weather-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-dashboard',
+  imports: [],
+  templateUrl: './weather-dashboard.html',
+  styleUrl: './weather-dashboard.scss',
+})
+export class WeatherDashboard {
+
+}
